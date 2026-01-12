a4d157d3c4989b2c466305b290e00e75bd98cf62
pa.jadhav <jadhavpankajjanardan@gmail.com>
2026-01-13 02:12:05 +0530

    created angular project for matrimonial

---
+++ src/app/app.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+  protected readonly title = signal('client');
+
+    ngOnInit(): void {
+    this.http.get('http://localhost:5172/api/profile/me').subscribe({
+      next: (response) => {
+        console.log('API is healthy:', response);
+      },
+      error: (error) => {
+        console.error('API health check failed:', error);
+      }
+    });
+  }
+}
