d17ae35079a43c309276f0122e5a9be71568afc8
pa.jadhav <jadhavpankajjanardan@gmail.com>
2026-01-18 02:36:51 +0530

    udpated code for portfolio

---
+++ src/nav/nav.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  imports: [],
+  templateUrl: './nav.html',
+  styleUrl: './nav.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Nav {
+
+}
