787e41f209a914a6e05b31ade41df3aa7e08699f
pa.jadhav <jadhavpankajjanardan@gmail.com>
2026-01-18 02:22:42 +0530

    added code for the header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Nav } from '../nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements OnInit {
-
   private http = inject(HttpClient);
   protected readonly title = signal('client');
-
-    ngOnInit(): void {
-    this.http.get('http://localhost:5172/api/profile/me').subscribe({
-      next: (response) => {
-        console.log('API is healthy:', response);
+  protected profiles: any;
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:5172/api/profile/profiles').subscribe({
+      next: (profiles) => {
+        this.profiles = profiles;
       },
-      error: (error) => {
-        console.error('API health check failed:', error);
-      }
+      error: (err) => {
+        console.error('Failed to load profiles:', err);
+      },
     });
   }
 }
